39369ad8d142a910c1544929a552dd09902cb508
automation_Check_in <duchuy4561232005@gmail.com>
2026-08-15 21:49:09 +0700

    modified:   Listings/main.map 	modified:   Objects/main.axf 	modified:   Objects/main.build_log.htm 	modified:   Objects/main.d 	modified:   Objects/main.htm 	modified:   Objects/main.lnp 	modified:   Objects/main.o 	modified:   Objects/main_Target 1.dep 	modified:   main.uvguix.ZoHuy 	modified:   main.uvoptx 	modified:   main.uvprojx
    Untracked files:
    	Objects/delay.d
    	Objects/delay.o
    	Objects/dma.d
    	Objects/dma.o
    	Objects/dshot.d
    	Objects/dshot.o
    	Objects/esekf.d
    	Objects/esekf.o
    	Objects/i2c_master.d
    	Objects/i2c_master.o
    	Objects/imu_bmi270.d
    	Objects/imu_bmi270.o
    	Objects/init.d
    	Objects/init.o
    	Objects/rasp_com.d
    	Objects/rasp_com.o
    	Objects/test_bmi270.d
    	Objects/test_bmi270.o
    	Objects/test_i2c.d
    	Objects/test_i2c.o
    	Objects/timer.d
    	Objects/timer.o
    	Objects/usart.d
    	Objects/usart.o
    	lib/dma.c
    	lib/i2c_master.c
    	lib/inc/dma.h
    	lib/inc/i2c_master.h
    	lib/inc/timer.h
    	lib/inc/usart.h
    	lib/timer.c
    	lib/usart.c
    	main/
    	test/

--- Objects/main.d
+++ Objects/main.d
@@ -4,4 +4,4 @@
   E:\Keli_v5\Pack\Keil\STM32H7xx_DFP\3.1.1\Drivers\CMSIS\Device\ST\STM32H7xx\Include\system_stm32h7xx.h \
   main\system\init.h lib\inc\clock.h lib\inc\gpio.h lib\inc\dshot.h \
   lib\inc\usart.h lib\inc\dma.h test\inc\test_i2c.h lib\inc\i2c_master.h \
-  E:\Keli_v5\ARM\ARMCLANG\Bin\..\include\string.h
+  test\inc\test_bmi270.h E:\Keli_v5\ARM\ARMCLANG\Bin\..\include\string.h
